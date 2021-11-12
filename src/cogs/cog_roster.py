bbe8a7655221010bd9b1b5452328b43a98cf9030
= <simonbj@vcu.edu>
2021-11-12 12:28:28 -0500

    Added some extra utility commands

--- src/cogs/cog_roster.py
+++ src/cogs/cog_roster.py
@@ -34,7 +34,7 @@ class RosterCog(commands.Cog):
                description='Replies with a table of all the users enlisted for a specific war',
                **cfg.cmd_cfg)
     async def get_enlisted(self, ctx):
-        if not check_permission(ctx, Perm.WAR_ROSTER):
+        if not await check_permission(ctx, Perm.WAR_ROSTER):
             return
         war, _ = await select_war(self.state, ctx, 'Select the war to get the enlistment roster for',
                                   allow_multiple=False,
@@ -58,7 +58,7 @@ class RosterCog(commands.Cog):
         description='Generates a CSV file with a table of everyone who signed up (Can be imported into excel).',
         **cfg.cmd_cfg)
     async def download_enlisted(self, ctx):
-        if not check_permission(ctx, Perm.WAR_ROSTER):
+        if not await check_permission(ctx, Perm.WAR_ROSTER):
             return
         war, _ = await select_war(self.state, ctx, 'Select the war to get the enlistment roster for',
                                   allow_multiple=False)
@@ -74,7 +74,7 @@ class RosterCog(commands.Cog):
                description='Flags a war as ended, disabling the ability to sign up for it',
                **cfg.cmd_cfg_mod)
     async def end_war(self, ctx):
-        if not check_permission(ctx, Perm.WAR_END):
+        if not await check_permission(ctx, Perm.WAR_END):
             return
         war, msg = await select_war(self.state, ctx, 'Select the war to end', allow_multiple=False)
         if war is not None:
@@ -93,7 +93,7 @@ class RosterCog(commands.Cog):
                description='Reposts the war notification',
                **cfg.cmd_cfg_elev)
     async def repost_war(self, ctx):
-        if not check_permission(ctx, Perm.WAR_POST):
+        if not await check_permission(ctx, Perm.WAR_POST):
             return
         wars, _ = await select_war(self.state, ctx, 'Select war', allow_multiple=True)
         for war in wars:
@@ -112,7 +112,7 @@ class RosterCog(commands.Cog):
                description='posts a war notification in your current channel',
                **cfg.cmd_cfg_elev)
     async def post_war(self, ctx):
-        if not check_permission(ctx, Perm.WAR_POST):
+        if not await check_permission(ctx, Perm.WAR_POST):
             return
         wars, _ = await select_war(self.state, ctx, 'Select war', allow_multiple=True)
         for war in wars:
@@ -123,7 +123,7 @@ class RosterCog(commands.Cog):
                description='posts a war enlistment button in your current channel',
                **cfg.cmd_cfg_elev)
     async def post_enlist(self, ctx):
-        if not check_permission(ctx, Perm.WAR_POST):
+        if not await check_permission(ctx, Perm.WAR_POST):
             return
         wars, msg = await select_war(self.state, ctx, 'Select war', allow_multiple=True)
         btns = []
