d40c852e0182807a4eb54013df7aeb8d9c75d032
= <simonbj@vcu.edu>
2021-11-23 07:52:53 -0500

    dfs

--- src/views/roster.py
+++ src/views/roster.py
@@ -29,7 +29,7 @@ def _group_by_role(roster: list, absent: list):
 def create_roster_embed(names, absent, state, title=None, embed=None, abrv_line=False):
     roster, absent = _get_users_from_names(names, absent, state.users)
     groups = _group_by_role(roster, absent)
-    
+
     if embed is None:
         embed = Embed(title='War Enlistment', description=f'{title}')
     fc = 0
@@ -42,7 +42,7 @@ def create_roster_embed(names, absent, state, title=None, embed=None, abrv_line=
             enl: Enlistment = enl
             if enl is None:
                 continue
-            val = '> ' + str(enl.roster_line(abrv_line)) + '\n'
+            val = f'> [{str(enl.roster_line(abrv_line))}](link "{enl.company}, {enl.level}")\n'
             if len(value) + len(val) > 1024:
                 embed.add_field(name=f'{key} {"" if idx == 0 else idx}', value=value, inline=abrv_line)
                 value = ''
